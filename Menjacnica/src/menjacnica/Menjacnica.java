a8f6f2ea9636480e61da842ca715333d3af85def
ivonaH <ivonaheldrih@gmail.com>
2020-03-02 10:49:06 +0100

    Dodata klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.Date;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValuteZaOdredjeniDan(Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteZaOdredjeniDan(Valuta valuta, ValutaNaDan valutaNaDan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pronadjiKursValteNaOredjeniDan(Valuta valuta, Date datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
