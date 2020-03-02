a8173f21a43aaad918cbeedd17d22bc318710bec
ivonaH <ivonaheldrih@gmail.com>
2020-03-02 10:29:34 +0100

    Dodat hashCode, equals i toString klasama ValutaNaDan i Valuta

--- Menjacnica/src/menjacnica/ValutaNaDan.java
+++ Menjacnica/src/menjacnica/ValutaNaDan.java
@@ -40,4 +40,46 @@ public class ValutaNaDan {
 		this.kupovniKurs = kupovniKurs;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + kupovniKurs;
+		result = prime * result + prodajniKurs;
+		result = prime * result + srednjiKurs;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ValutaNaDan other = (ValutaNaDan) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (kupovniKurs != other.kupovniKurs)
+			return false;
+		if (prodajniKurs != other.prodajniKurs)
+			return false;
+		if (srednjiKurs != other.srednjiKurs)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ValutaNaDan [datum=" + datum + ", prodajniKurs=" + prodajniKurs + ", srednjiKurs=" + srednjiKurs
+				+ ", kupovniKurs=" + kupovniKurs + "]";
+	}
+
+	
+	
 }
