0485889b0d35e6031da8281edbd41f55f73433ea
ivonaH <ivonaheldrih@gmail.com>
2020-03-02 11:04:40 +0100

    Commit sa Master grane

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -9,18 +9,18 @@ public class Menjacnica implements MenjacnicaInterfejs {
 	@Override
 	public void dodajKursValuteZaOdredjeniDan(Valuta valuta, ValutaNaDan valutaNaDan) {
 		
-		
+		//tekst 17717271
 	}
 
 	@Override
 	public void obrisiKursValuteZaOdredjeniDan(Valuta valuta, ValutaNaDan valutaNaDan) {
-		
+		//tekst 1212818
 		
 	}
 
 	@Override
 	public ValutaNaDan pronadjiKursValteNaOredjeniDan(Valuta valuta, Date datum) {
-		// TODO Auto-generated method stub
+		// neki tekst
 		return null;
 	}
 
