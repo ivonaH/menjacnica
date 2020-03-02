e3604359d2670f4c3b1f1081670161a01e5f636d
ivonaH <ivonaheldrih@gmail.com>
2020-03-02 11:02:14 +0100

    Implementirane metode u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -8,21 +8,24 @@ public class Menjacnica implements MenjacnicaInterfejs {
 
 	@Override
 	public void dodajKursValuteZaOdredjeniDan(Valuta valuta, ValutaNaDan valutaNaDan) {
-		
-		
+		valuta.getValutaNaDan().add(valutaNaDan);
+
 	}
 
 	@Override
 	public void obrisiKursValuteZaOdredjeniDan(Valuta valuta, ValutaNaDan valutaNaDan) {
-		
-		
+		valuta.getValutaNaDan().remove(valutaNaDan);
+
 	}
 
 	@Override
 	public ValutaNaDan pronadjiKursValteNaOredjeniDan(Valuta valuta, Date datum) {
 		// TODO Auto-generated method stub
+		for (ValutaNaDan vd : valuta.getValutaNaDan()) {
+			if (vd.getDatum().equals(datum))
+				return vd;
+		}
 		return null;
 	}
-
 
 }
